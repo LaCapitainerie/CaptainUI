c3a66485b9d61b647cb80a9890bf96a4e80db1db
LaCapitainerie <hugo.antreassian@gmail.com>
2024-12-02 15:16:31 +0100

    Update Way to import files

--- cli.js
+++ cli.js
@@ -28,29 +28,36 @@ const downloadComponent = async () => {
     "file-dialog": [
       "file-dialog.tsx",
     ],
-    "data-table": [
-      "row-type/data-table-row-badge.tsx",
-      "row-type/data-table-row-boolean.tsx",
-      "row-type/data-table-row-currency.tsx",
-      "row-type/data-table-row-date.tsx",
-      "row-type/data-table-row-enum.tsx",
+    "data-table": {
+      'src/components/captainui/': [
+        "row-type/data-table-row-badge.tsx",
+        "row-type/data-table-row-boolean.tsx",
+        "row-type/data-table-row-currency.tsx",
+        "row-type/data-table-row-date.tsx",
+        "row-type/data-table-row-enum.tsx",
 
-      "data-table-column-header.tsx",
-      "data-table-faceted-filter.tsx",
-      "data-table-form.tsx",
-      "data-table-pagination.tsx",
-      "data-table-row-actions.tsx",
-      "data-table-skeleton.tsx",
-      "data-table-toolbar.tsx",
-      "data-table-view-options.tsx",
-      "data-table.tsx",
-      
-    ],
+        "data-table-column-header.tsx",
+        "data-table-faceted-filter.tsx",
+        "data-table-form.tsx",
+        "data-table-pagination.tsx",
+        "data-table-row-actions.tsx",
+        "data-table-skeleton.tsx",
+        "data-table-toolbar.tsx",
+        "data-table-view-options.tsx",
+        "data-table.tsx",
+
+      ],
+
+      'utils/': [
+        "utils.ts",
+      ],
+    }
+
   }
 
   const args = process.argv.slice(2);
   const componentName = args.slice(1);
-  
+
   for (const component of componentName) {
 
     const baseUrl = `https://raw.githubusercontent.com/LaCapitainerie/CaptainUI/refs/heads/main/${component}/`;
@@ -60,32 +67,36 @@ const downloadComponent = async () => {
       return;
     }
 
-    const destDir = path.resolve(process.cwd(), 'src/components/captainui/' + component);
+    for (const [destDirValue, listFiles] of Object.entries(files[component])) {
+      if (typeof destDirValue[1] === 'string') {
+        const destDir = path.resolve(process.cwd(), destDirValue + component);
 
-    if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir, { recursive: true });
+        if (!fs.existsSync(destDir)) {
+          fs.mkdirSync(destDir, { recursive: true });
+        }
+
+        for (const file of listFiles) {
+          const url = `${baseUrl}${file}`;
+          const dest = path.join(destDir, file);
+          const dir = path.dirname(dest);
+
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+          }
+
+          try {
+            console.log(`Downloading ${file}...`);
+            await downloadFile(url, dest);
+          } catch (err) {
+            console.error(`Failed to download ${file}:`, err);
+          }
+        }
+      }
     }
 
-    for (const file of files[component]) {
-      const url = `${baseUrl}${file}`;
-      const dest = path.join(destDir, file);
-      const dir = path.dirname(dest);
-  
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-      }
-  
-      try {
-        console.log(`Downloading ${file}...`);
-        await downloadFile(url, dest);
-      } catch (err) {
-        console.error(`Failed to download ${file}:`, err);
-      }
-    }
-    
   }
 
-  
+
 };
 
 downloadComponent();
