da03a1c9a44761b98a065e780529b8086a65d56b
LaCapitainerie <hugo.antreassian@gmail.com>
2025-01-06 11:25:43 +0100

    Add Prisma Auth

--- cli.js
+++ cli.js
@@ -3,8 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-const { createHmac } = require('crypto');
-const { updatePrismaSchema } = require('./prisma');
+const { parsePrismaSchema } = require('@loancrate/prisma-schema-parser');
 
 const downloadFile = (url, dest) => {
   return new Promise((resolve, reject) => {
@@ -26,6 +25,67 @@ const downloadFile = (url, dest) => {
     });
   });
 };
+
+function generatePrismaSchema(json) {
+  let schema = '';
+
+  // Generate datasource
+  const datasource = json.datasource[0];
+  schema += `datasource db {\n  provider = "${datasource.provider}"\n  url      = "${datasource.url}"\n}\n\n`;
+
+  // Generate generator
+  const generator = json.generator[0];
+  schema += `generator client {\n  provider = "${generator.provider}"\n  output   = "${generator.output}"\n}\n\n`;
+
+  // Generate models
+  json.model.forEach((model) => {
+    schema += `model ${model.name} {\n`;
+    model.fields.forEach((field) => {
+      schema += `  ${field.name} ${field.type}`;
+      if (field.id) schema += ' @id';
+      if (field.unique) schema += ' @unique';
+      if (field.default) schema += ` @default(${field.default})`;
+      if (field.updatedAt) schema += ' @updatedAt';
+      if (field.optional) schema += '?';
+      if (field.relation) {
+        schema += ` @relation(fields: [${field.relation.fields.join(', ')}], references: [${field.relation.references.join(', ')}]`;
+        if (field.relation.onDelete) schema += `, onDelete: ${field.relation.onDelete}`;
+        schema += ')';
+      }
+      schema += '\n';
+    });
+    
+    schema +=  model["@@id"] ? `  @@id([${model["@@id"].join(', ')}])\n` : '';
+
+    schema += `}\n\n`;
+  });
+
+  return schema;
+}
+
+function updatePrismaSchema(prismaSchemaPath, jsonSchema) {
+
+  // Check if the Prisma schema file exists
+  if (!fs.existsSync(prismaSchemaPath)) {
+    fs.mkdirSync(path.resolve(process.cwd(), "prisma"), { recursive: true });
+    fs.writeFileSync(prismaSchemaPath, "");
+    console.log("Prisma schema file created.");
+  }
+
+  // Read the Prisma schema file
+  const prismaSchema = fs.readFileSync(prismaSchemaPath, "utf-8");
+
+  // Parse the Prisma schema
+  const CurrentSchema = parsePrismaSchema(prismaSchema);
+
+  // Merge the new schema with the current schema
+  const NewSchema = { ...CurrentSchema, ...jsonSchema };
+
+  // Write the new schema to the Prisma schema file
+  fs.writeFileSync(prismaSchemaPath, generatePrismaSchema(NewSchema));
+
+  console.log("Prisma schema updated.");
+}
 
 const downloadComponent = async () => {
 
@@ -74,8 +134,9 @@ const downloadComponent = async () => {
   const NpmList = Components.map(componentName => files[componentName]["npm"] || []).flat();
   const ShadcnList = Components.map(componentName => files[componentName]["shadcn"] || []).flat();
   const PrismaList = Components.map(componentName => files[componentName]["prisma"] || []).flat();
+  const FilesList = Components.map(componentName => files[componentName]["files"] || []).flat();
 
-  
+
   // Dependencies installation
   // npm i ...
   for (const dept of NpmList) {
@@ -117,54 +178,60 @@ const downloadComponent = async () => {
 
   // Prisma installation
   if (installPrisma) {
-    updatePrismaSchema('prisma/schema.prisma', PrismaList);
+    console.log(`📦 Prisma installation`);
+    console.log(path.resolve(process.cwd(), 'prisma/schema.prisma'));
+    updatePrismaSchema(path.resolve(process.cwd(), 'prisma/schema.prisma'), PrismaList[0]);
   }
 
   // Files installation
-  for (const componentName of Components) {
+  if (FilesList.length > 0) {
+    console.log(`📦 Files installation`);
+    if (isFactice) return '📦 Factice mode activated, no files will be downloaded';
+    for (const componentName of Components) {
 
-    const GithubUrl = `https://raw.githubusercontent.com/LaCapitainerie/CaptainUI/refs/heads/main/components`;
+      const GithubUrl = `https://raw.githubusercontent.com/LaCapitainerie/CaptainUI/refs/heads/main/components`;
 
-    if (!files[componentName]) {
-      console.error(`Component ${componentName} not found`);
-      return;
-    }
+      if (!files[componentName]) {
+        console.error(`Component ${componentName} not found`);
+        return;
+      }
 
-    // Files installation
-    // fetch ...
-    for (const [ClientFolderInstallation, listFiles] of Object.entries(files[componentName]["files"])) {
-      if (typeof ClientFolderInstallation[1] === 'string') {
-        if (isFactice) continue;
-        const PwdClientFolderInstallation = path.resolve(process.cwd(), ClientFolderInstallation);
+      // Files installation
+      // fetch ...
+      for (const [ClientFolderInstallation, listFiles] of Object.entries(files[componentName]["files"])) {
+        if (typeof ClientFolderInstallation[1] === 'string') {
+          if (isFactice) continue;
+          const PwdClientFolderInstallation = path.resolve(process.cwd(), ClientFolderInstallation);
 
-        if (!fs.existsSync(PwdClientFolderInstallation)) {
-          fs.mkdirSync(PwdClientFolderInstallation, { recursive: true });
-        }
-
-        for (const RepoFileURL of listFiles) {
-
-          const fetchUrl = `${GithubUrl}/${RepoFileURL}`;
-          const ClientFinalDestination = path.join(PwdClientFolderInstallation, RepoFileURL);
-          const dir = path.dirname(ClientFinalDestination);
-
-          if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
+          if (!fs.existsSync(PwdClientFolderInstallation)) {
+            fs.mkdirSync(PwdClientFolderInstallation, { recursive: true });
           }
 
-          // console.log({
-          //   "ClientFinalDestination": ClientFinalDestination, 
-          //   "Pwd": process.cwd(),
-          //   "ClientFolderInstallation": ClientFolderInstallation,
-          //   "RepoFileURL": RepoFileURL,
+          for (const RepoFileURL of listFiles) {
 
-          //   "ComponentName": componentName,
-          //   "PwdClientFolderInstallation": PwdClientFolderInstallation,
-          // });
+            const fetchUrl = `${GithubUrl}/${RepoFileURL}`;
+            const ClientFinalDestination = path.join(PwdClientFolderInstallation, RepoFileURL);
+            const dir = path.dirname(ClientFinalDestination);
 
-          try {
-            await downloadFile(fetchUrl, ClientFinalDestination);
-          } catch (error) {
-            console.error(`\r❌ Error on shipping component at ${fetchUrl}, reason : ${error}\n`);
+            if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir, { recursive: true });
+            }
+
+            // console.log({
+            //   "ClientFinalDestination": ClientFinalDestination, 
+            //   "Pwd": process.cwd(),
+            //   "ClientFolderInstallation": ClientFolderInstallation,
+            //   "RepoFileURL": RepoFileURL,
+
+            //   "ComponentName": componentName,
+            //   "PwdClientFolderInstallation": PwdClientFolderInstallation,
+            // });
+
+            try {
+              await downloadFile(fetchUrl, ClientFinalDestination);
+            } catch (error) {
+              console.error(`\r❌ Error on shipping component at ${fetchUrl}, reason : ${error}\n`);
+            }
           }
         }
       }
